8c9e1f254424fca84e7d2468c8a86c728af3380d
Owlynn <contact@jessalynn.fr>
2022-07-08 16:33:38 +0200

    Création des fichiers de base du projet

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './App.css';
+
+class App extends React.Component{
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return <h1> Hello</h1>
+  }
+}
+
+export default App;
